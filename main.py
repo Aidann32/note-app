a608fba90c3e60bdaff2a9c923619b3b499db5d5
dev12 <46024391+Aidann32@users.noreply.github.com>
2022-06-18 02:46:56 +0500

    Adding frontend code

--- main.py
+++ main.py
@@ -2,11 +2,4 @@ import eel
 
 eel.init('webinterface', allowed_extensions=['.js', '.html'])
 
-@eel.expose
-def print_sum_py(a, b):
-    print(str(a + b))
-
-print(eel._exposed_functions)
-
-
-eel.start('templates/main.html', port=8001)
+eel.start('templates/main_tab.html', port=8001, jinja_templates='templates', geometry={'position': (0,0)})
