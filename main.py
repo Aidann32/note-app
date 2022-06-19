563a4360de741f3d37c2b12b8fc1d6e91199b0c8
dev12 <46024391+Aidann32@users.noreply.github.com>
2022-06-19 23:21:07 +0500

    More frontend

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import eel
 
 eel.init('webinterface', allowed_extensions=['.js', '.html'])
 
-eel.start('templates/main_tab.html', port=8001, jinja_templates='templates', geometry={'position': (0,0)})
+eel.start('templates/base.html', port=8001, jinja_templates='templates', geometry={'position': (0,0)})
